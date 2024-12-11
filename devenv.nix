513319bcee791189c453b2ab4aa56b3fbe70c2c4
Brandon Conway <brandoncc@gmail.com>
2024-12-10 21:58:54 -0800

    Add javascript to devenv so LSPs can use npm

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,10 @@
 {
   packages = [ pkgs.git pkgs.foreman pkgs.libyaml.dev ];
 
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
 
   languages.ruby = {
     enable = true;

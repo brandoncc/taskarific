baa4f921c2aaf193643e68d641c934a4399f3e17
Brandon Conway <brandoncc@gmail.com>
2024-12-10 21:58:11 -0800

    Tasks can be added, modified, deleted
    Things are broken, the site is ugly, tests don't pass. The goal is just
    to get it working, then I can work on cleaning things up.

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,74 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ show edit update destroy ]
+
+  # GET /tasks or /tasks.json
+  def index
+    @new_task = Task.new
+    @tasks = Task.all
+  end
+
+  # GET /tasks/1 or /tasks/1.json
+  def show
+  end
+
+  # GET /tasks/new
+  def new
+    @task = Task.new
+  end
+
+  # GET /tasks/1/edit
+  def edit
+  end
+
+  # POST /tasks or /tasks.json
+  def create
+    @new_task = Task.new(task_params)
+
+    respond_to do |format|
+      @new_task.save
+      @tasks = Task.all
+
+      if @new_task.persisted?
+        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
+        format.json { render :index, status: :created, location: tasks_path }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @new_task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tasks/1 or /tasks/1.json
+  def update
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1 or /tasks/1.json
+  def destroy
+    @task.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to tasks_path, status: :see_other, notice: "Task was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_task
+      @task = Task.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def task_params
+      params.expect(task: [ :name, :description, :parent_id, :status, :completed ])
+    end
+end
